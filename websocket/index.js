ab5714bc810b72f5e8be95737a9820f9cb65dca9
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-20 14:45:59 +0200

    open websocket connection
    set up jwt authorization

---
+++ websocket/index.js
@@ -0,0 +1,34 @@
+const WebSocket = require('ws');
+const tokenService = require('../service/tokenService');
+const WSEvent = require('./wsEvent')
+const queryString = require('querystring');
+
+module.exports = (server) => {
+    const websocketServer = new WebSocket.Server({
+        noServer: true,
+    });
+
+
+    server.on("upgrade", (req, socket, head) => {
+        websocketServer.handleUpgrade(req, socket, head, (ws) => {
+            websocketServer.emit("connection", ws, req)
+        });
+    });
+
+    websocketServer.on('connection', async (ws,req)  => {
+        const [_path, params] = req?.url?.split("?");
+        const objParams = queryString.parse(params);
+        const access = await tokenService.validateAccess(objParams.accessToken);
+        if(!access) ws.close(1008, 'accessToken is invalid');
+
+        const arr = ['username' ,'roomId'];
+        arr.forEach(el => {
+            if(!objParams[el]) ws.close(1008, 'exist ' + el);
+            ws[el] = objParams[el];
+        })
+        const wsEvent = new WSEvent()
+        wsEvent.event(websocketServer,ws)
+
+    })
+    return websocketServer;
+}
