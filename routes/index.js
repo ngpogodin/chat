b69df405b9a79ec5b5ec0e73d4bdaa662a880644
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-20 15:49:28 +0200

    add room route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const Router = require('express');
 const router = new Router();
 const authRouter = require('./authRouter');
-const userRouter = require('./userRouter')
-
+const userRouter = require('./userRouter');
+const roomRouter = reqiore('./roomRouter');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.use('/', authRouter);
-router.use('/user', userRouter)
+router.use('/user', userRouter);
+router.use('/room',authMiddleware, roomRouter);
 
 
 module.exports = router;
