203437cf8008ad500cfaebf0cbf89ca32e10987a
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-20 13:14:01 +0200

    Set up middlewares,connect to db and start server

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const websocket = require('./websocket/index');
+const http = require("http");
+const mongoose = require('mongoose')
+const router = require('./routes/index')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
+mongoose.connect(process.env.DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log('mongoDB connected'));
+
+
+
+
+
+const server = app.listen(PORT, () => {
+    console.log('server start')
+})
+
+websocket(server)
