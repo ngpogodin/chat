0bd80c9d00b178221e6b7d0c9577c15326c15058
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-23 19:47:25 +0200

    change common error to ApiError

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,23 @@
+const ApiError = require('../exceptions/ApiError');
 const tokenService = require('../service/tokenService');
+
 
 
 module.exports = async function(req,res,next) {
     try{
         const authorizationHeader  = req.headers.authorization;
         if(!authorizationHeader) {
-            throw new Error('not auth')
+            throw ApiError.UnauthorizedError()
         }
 
         const token = authorizationHeader.split(' ')[1];
         if(!token) {
-            throw new Error('not auth')
+            throw ApiError.UnauthorizedError();
         }
 
         const userData = await tokenService.validateAccess(token);
         if(!userData) {
-            throw new Error('not auth')
+            throw ApiError.UnauthorizedError();
         }
         req.user = userData;
         next()
