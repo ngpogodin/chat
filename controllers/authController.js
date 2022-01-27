73a5373d25ccc53a1541b4df75b24b9cdf7a6987
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-27 16:35:46 +0200

    Add validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const userService = require('../service/userService')
+const userService = require('../service/userService');
+const {validationResult} = require('express-validator');
 
 
 class AuthController {
@@ -13,6 +14,11 @@ class AuthController {
     }
     async registration(req,res,next) {
         try{
+            const errors = validationResult(req);
+            console.log(errors)
+            if(!errors.isEmpty()) {
+                return res.status(400).json({errors: errors.array()})
+            }
             const userData = await userService.registration(req.body.username, req.body.password);
             res.cookie('refreshToken',userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly:true});
             res.json({userData})
