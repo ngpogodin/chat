853317724bc667ce0b2f79ef6780dc9965e6cd7f
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-27 16:37:30 +0200

    Create new method
    getById and getAllUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,44 @@
-const userModel = require('../models/userModel')
-
+const ApiError = require('../exceptions/ApiError');
+const userModel = require('../models/userModel');
+const UserDto = require('../dtos/user-dto');
+const GetService = require('../service/getService');
 
 class UserController {
     async getUser(req,res,next) {
         try{
             const username = req.params.username
-            const user = await userModel.findOne({username}).select('-hash -_id -__v');
-            res.json(user)
+            const userDoc = await userModel.findOne({username});
+            if(!userDoc) throw ApiError.BadRequest('no such user');
+            if(req.user.username === userDoc.username) return res.json(new UserDto(userDoc));
+            return res.json(new UserDto(userDoc).isUser());
         }catch(e) {
-            next(e)
+            next(e);
         }
+    }
+
+    async getById(req,res,next) {
+        try{
+            const userId = req.params.id;
+            const user = await userModel.findById(userId);
+            if(!user) throw ApiError.BadRequest('no such user');
+            if(req.user.username === userDoc.username) return res.json(new UserDto(userDoc));
+            return res.json(new UserDto(userDoc).isUser());
+        }catch(e) {
+            next(e);
+        }
+    }
+    async getAllUsers(req,res,next) {
+        try{
+            const {limit,offset} = req.query;
+
+            const arr = await new GetService(userModel).getAll(limit,offset)
+    
+            const users = arr.map(user => new UserDto(user).isUser());
+            res.json({length: users.length, users});
+        }catch(e) {
+            next(e);
+        }
+
     }
 }
 
