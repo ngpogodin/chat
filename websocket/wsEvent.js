a3754e248eff73ffa844f48b33aeba3d9a1fbf6a
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-27 17:13:32 +0200

    add event pong,update event close and msg
    Set interval which sending ping .
    When connection close members of the room receive online msg

--- websocket/wsEvent.js
+++ websocket/wsEvent.js
@@ -1,19 +1,33 @@
 const SwitchCases = require('./wsSwitch');
+const wsService = require('./wsService');
 
 
 
 class WSEvent {
     constructor() {
-        this.switchCases = new SwitchCases()
+        this.switchCases = new SwitchCases();
+        this.interval
     }
+
     event(wss,ws) {
         ws.on('close', (code,reason) => {
             console.log(code, reason)
+            clearInterval(this.interval)
+            const online = wsService.onlineInRoom(wss,ws);
+            wsService.sendAll(wss,ws,{method: 'online', online});
         })
         ws.on("message", (message) => {
             const msg = JSON.parse(message);
-            this.switchCases.switch(wss, ws, msg)
+
+            if(msg.method === 'enter') {
+                this.interval = wsService.setPing(ws);
+            }
+            
+            this.switchCases.switch(wss, ws, msg);
         });
+        ws.on('pong', () => {
+            ws.isAlive = true;
+        } )
     }
 }
 
