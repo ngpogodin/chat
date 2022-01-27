8830db56b03319ed88334282a360b3377c232c35
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-27 17:17:25 +0200

    add methods wich set interval for ping

--- websocket/wsService.js
+++ websocket/wsService.js
@@ -8,6 +8,23 @@ class WSService {
             }
         });
     }
+    onlineInRoom(wss,ws) {
+        const onlineMembers = [];
+        wss.clients.forEach(client => {
+            if(client.roomId === ws.roomId) {
+                onlineMembers.push(client.user.username);
+            }
+        })
+        return onlineMembers
+    }
+    setPing(ws) {
+        return setInterval( () => {
+            console.log('ws')
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();        
+       }, 30000);
+    }
 }
 
 module.exports = new WSService();
