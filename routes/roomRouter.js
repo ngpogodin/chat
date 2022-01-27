cdf5c4b72e8c48fda369ab92e890866111c3c137
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-27 16:40:19 +0200

    Add middleware that check user is admin

--- routes/roomRouter.js
+++ routes/roomRouter.js
@@ -4,12 +4,12 @@ const roomController = require('../controllers/roomController');
 const isAdminMiddleware = require('../middlewares/isAdminMiddleware');
 
 
-router.post('/' ,roomController.createRoom);
+router.post('/',roomController.createRoom);
 router.get('/',roomController.getAllRooms);
 router.get('/:id', roomController.getByIdOrName);
-router.post('/invite',isAdminMiddleware, roomController.inviteToRoom); //add middleware
-router.post('/remove',isAdminMiddleware, roomController.removeMember); //add middleware
-router.delete('/:id',isAdminMiddleware, roomController.deleteRoom); //add middleware
+router.post('/invite',isAdminMiddleware, roomController.inviteToRoom); 
+router.post('/remove',isAdminMiddleware, roomController.removeMember); 
+router.delete('/:id',isAdminMiddleware, roomController.deleteRoom); 
 
 
 module.exports = router
