b130d0eb73c56f60ab460e0ab23969a0b7e62890
Andrey Pogodin <ngpogodin@gmail.com>
2022-01-27 16:39:56 +0200

    Add validation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,15 @@
 const Router = require('express');
 const router = new Router();
 const authController = require('../controllers/authController');
+const {body} = require('express-validator');
 
 
 
-router.post('/login', authController.login);
-router.post('/registration', authController.registration);
+router.post('/login',authController.login);
+router.post('/registration',
+                            body('username').isLength({min:4,max:15}),
+                            body('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/), 
+                            authController.registration);
 router.get('/refresh', authController.refresh)
 
 
